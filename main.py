740f44ef6f0755106cafc0580204b238a8028740
albin1923 <108322704+albin1923@users.noreply.github.com>
2025-07-27 20:48:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,140 @@
+import pygame
+import sys
+import random
+import os
+def draw_base():
+    screen.blit(base_surface, (base_x_pos, 450))
+    screen.blit(base_surface, (base_x_pos + 288, 450))
+
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop=(350, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(350, random_pipe_pos - 150))
+    return bottom_pipe, top_pipe
+
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= pipe_speed
+    return pipes
+
+
+def remove_pipes(pipes):
+    for pipe in pipes:
+        if pipe.centerx <= -300:
+            pipes.remove(pipe)
+    return pipes
+
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 512:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
+
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+
+    if bird_rect.top <= -50 or bird_rect.bottom >= 450:
+        return False
+
+    return True
+
+
+def rotate_bird(bird):
+    new_bird = pygame.transform.rotozoom(bird, -bird_movement * 3, 1)
+    return new_bird
+
+
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect = new_bird.get_rect(center=(50, bird_rect.centery))
+    return new_bird, new_bird_rect
+
+
+pygame.init()
+screen = pygame.display.set_mode((288, 512))
+clock = pygame.time.Clock()
+# Game Variables
+gravity = 0.25
+bird_movement = 0
+pipe_speed = 3
+game_active = True
+# Assets loading
+BASE_DIR = os.path.dirname(r'C:\Users\USER\Documents\flappybird\flappybird-pygame\assets')
+bg_surface = pygame.image.load(os.path.join(BASE_DIR, 'background-night.png')).convert()
+base_surface = pygame.image.load(os.path.join(BASE_DIR, 'base.png')).convert()
+base_x_pos = 0
+bird_downflap = pygame.image.load(os.path.join(BASE_DIR, 'redbird-downflap.png')).convert_alpha()
+bird_midflap = pygame.image.load(os.path.join(BASE_DIR, 'redbird-midflap.png')).convert_alpha()
+bird_upflap = pygame.image.load(os.path.join(BASE_DIR, 'redbird-upflap.png')).convert_alpha()
+bird_frames = [bird_downflap, bird_midflap, bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
+bird_rect = bird_surface.get_rect(center=(50, 256))
+BIRDFLAP = pygame.USEREVENT + 1
+pygame.time.set_timer(BIRDFLAP, 200)
+pipe_surface = pygame.image.load(os.path.join(BASE_DIR, 'pipe-red.png')).convert()
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 800)
+pipe_height = [200, 300, 400]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game_active:
+                bird_movement = 0
+                bird_movement -= 6
+            if event.key == pygame.K_SPACE and not game_active:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (50, 256)
+                bird_movement = 0
+
+        if event.type == SPAWNPIPE:
+            pipe_list.extend(create_pipe())
+
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+
+            bird_surface, bird_rect = bird_animation()
+
+    # background_image
+    screen.blit(bg_surface, (0, 0))
+
+    if game_active:
+        # Bird
+        bird_movement += gravity
+        rotated_bird = rotate_bird(bird_surface)
+        bird_rect.centery += bird_movement
+        screen.blit(rotated_bird, bird_rect)
+        game_active = check_collision(pipe_list)
+
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        pipe_list = remove_pipes(pipe_list)
+        draw_pipes(pipe_list)
+    else:
+        None
+
+    # Moving Floor
+    base_x_pos -= 1
+    draw_base()
+    if (base_x_pos <= -288):
+        base_x_pos = 0
+
+    pygame.display.update()
+    clock.tick(60)                                       
